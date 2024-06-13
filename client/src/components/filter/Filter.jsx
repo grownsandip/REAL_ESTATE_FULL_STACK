03fa74093274a5fbe141a6bce3152d461dc31f60
Sandip <roysandip33290@gmail.com>
2024-06-13 11:04:53 +0530

    improved the card and filtering process using search query

--- client/src/components/filter/Filter.jsx
+++ client/src/components/filter/Filter.jsx
@@ -1,29 +1,56 @@
+import { useState } from "react";
 import "./filter.scss";
+import { useSearchParams } from "react-router-dom";
 function Filter() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [query, setQuery] = useState({
+    type: searchParams.get("type") || "",
+    city: searchParams.get("city") || "",
+    maxPrice: searchParams.get("maxPrice") || 1000000,
+    minPrice: searchParams.get("minPrice") || 0,
+    property: searchParams.get("property") || "",
+    bedroom: searchParams.get("bedroom") || 1,
+  });
+  const handleChange = (e) => {
+    setQuery({
+      ...query,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleFilter = () => {
+    setSearchParams(query);
+  };
   return (
     <div className="filter">
       <h1>
-        search reasults for<b>London</b>
+        search reasults for<b>{searchParams.get("city")}</b>
       </h1>
       <div className="top">
         <div className="item">
           <label htmlFor="city">Location</label>
-          <input type="text" id="city" name="city" placeholder="loaction" />
+          <input
+            type="text"
+            id="city"
+            name="city"
+            placeholder="location"
+            onChange={handleChange}
+            defaultValue={query.city}
+          />
         </div>
       </div>
       <div className="bottom">
         <div className="item">
           <label htmlFor="type">Type</label>
-          <select name="type" id="type">
-          <option value="any">Any</option>
+          <select name="type" id="type" onChange={handleChange} defaultValue={query.type}>
+            <option value="any">Any</option>
             <option value="but">Buy</option>
             <option value="rent">Rent</option>
           </select>
         </div>
         <div className="item">
           <label htmlFor="property">Property</label>
-          <select name="property" id="property">
-          <option value="any">Any</option>
+          <select name="property" id="property" onChange={handleChange} defaultValue={query.property}>
+            <option value="any">Any</option>
             <option value="Apartment">Apartment</option>
             <option value="House">House</option>
             <option value="Condo">Condo</option>
@@ -32,18 +59,39 @@ function Filter() {
         </div>
         <div className="item">
           <label htmlFor="minPrice">Min Price</label>
-          <input type="number" id="minPrice" name="minPrice" placeholder="any" />
+          <input
+            type="number"
+            id="minPrice"
+            name="minPrice"
+            placeholder="any"
+            onChange={handleChange}
+            defaultValue={query.minPrice}
+          />
         </div>
         <div className="item">
-        <label htmlFor="maxPrice">Max Price</label>
-          <input type="number" id="maxPrice" name="maxPrice" placeholder="any" />
+          <label htmlFor="maxPrice">Max Price</label>
+          <input
+            type="number"
+            id="maxPrice"
+            name="maxPrice"
+            placeholder="any"
+            onChange={handleChange}
+            defaultValue={query.maxPrice}
+          />
         </div>
         <div className="item">
           <label htmlFor="bedroom">Bed Room</label>
-          <input type="text" id="bedroom" name="bedroom" placeholder="any" />
+          <input
+            type="text"
+            id="bedroom"
+            name="bedroom"
+            placeholder="any"
+            onChange={handleChange}
+            defaultValue={query.bedroom}
+          />
         </div>
-        <button>
-            <img src="./images/search.png" alt="" />
+        <button onClick={handleFilter}>
+          <img src="./images/search.png" alt="" />
         </button>
       </div>
     </div>
